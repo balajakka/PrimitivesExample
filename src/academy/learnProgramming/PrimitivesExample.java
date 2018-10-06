dc51f45947c5837f9ace3884c2d71fec33ba357f
balajakka <jbkrao8281@gmail.com>
2018-10-06 18:20:03 +0100

    Primitives Examples created

---
+++ src/academy/learnProgramming/PrimitivesExample.java
@@ -0,0 +1,30 @@
+package academy.learnProgramming;
+
+/**
+ * Created by balajakka on 06/10/2018.
+ */
+public class PrimitivesExample {
+    public static void main(String[] args) {
+        long max=32145678989; // add L to fix this (you can add underscores to make reaadable
+        long n = _2; //fix it removing underscore prefix (add 23_00_0 as example)
+        //dont discuss octal and hexadecimal number
+
+        float myNumber= 2.56; //fix this by sufixing F to number
+        int myIntNumber = 2.56;
+        double before = 10_.25;  // underscore cannot be before decimal points
+        double after = 10._25;   //underscore cannot be before decimal points
+        double last = 10.25;_    //
+        double first = 10.25;   //
+
+        char ch = 'a';
+        char uniChar = '\u03A9';
+        char romanChar = '\u216';
+        //print char values
+
+        boolean myBoolean = true;
+        boolean myFalseBoolean = false;
+
+        boolean true = true;//fix this
+
+    }
+}
